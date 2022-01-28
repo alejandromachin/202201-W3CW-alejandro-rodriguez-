c786f9d80764516b130b8326716cc2683778aad4
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-28 19:49:30 +0100

    rendered a pageComponent div and a card in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,10 @@
+import PageComponent from "./components/PageComponent/PageComponent.js";
+import PokeComponent from "./components/PokeComponent/PokeComponent.js";
 
+const divContainer = document.querySelector(".main-container");
+
+new PageComponent(divContainer);
+
+const pageContainer = document.querySelector(".pageContainer");
+
+const pokeCard = new PokeComponent(pageContainer, "poke-card");
