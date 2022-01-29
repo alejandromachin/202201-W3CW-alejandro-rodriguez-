5028beb71abe7d6ce6060df15b29e5bc815c5b45
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-29 09:05:53 +0100

    created a class CardButtonComponent that instantiate a button

---
+++ public/js/components/CardButtonComponent/CardButtonComponent.js
@@ -0,0 +1,17 @@
+import Component from "../Component.js";
+
+class CardButtonComponent extends Component {
+  actionOnClick;
+  text;
+
+  constructor(parentElement, className, actionOnClick, text) {
+    super(parentElement, className, "button");
+    this.actionOnClick = actionOnClick;
+    this.text = text;
+    this.generateHTML();
+  }
+
+  generateHTML() {
+    this.element.textContent = this.text;
+  }
+}
